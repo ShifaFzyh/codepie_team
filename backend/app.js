531605e1fc59bea68da5707085c3af3845794248
ShifaFzyh <shifafau05@gmail.com>
2026-04-15 20:33:02 +0700

    memisahkan backend frontend dan install ulang nom

---
+++ backend/app.js
@@ -0,0 +1,75 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// Pastikan path ini benar. Jika app.js di folder backend, maka panggil ./
+const router = require('./routes/api'); 
+const db = require('./config/database');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Melayani file statis dari folder frontend yang sejajar dengan backend
+app.use(express.static(path.join(__dirname, '../frontend')));
+
+// Masukkan router API
+app.use(router);
+
+// Test koneksi database
+app.get("/test-koneksi", (req, res) =>{
+    db.query("SELECT 1", (err, result) => {
+        if (err) {
+            console.error("Gagal kueri:", err); // Lihat log di terminal
+            res.status(500).json({ message: "Koneksi database ke MySQL gagal"});
+        } else {
+            res.json({ message: "Koneksi database berhasil!", result: result });
+        }
+    });
+});
+
+// Halaman login
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/views/login.html'));
+});
+
+// Proses login
+app.post('/login', (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    // Cek data user
+    let role = "";
+
+    if(email === "admin@gmail.com" && password === "12345"){
+        role = "admin";
+    } else if(email === "editor@gmail.com" && password === "12345"){
+        role = "editor";
+    } else {
+        return res.send("Login gagal!");
+    }
+
+
+    // Redirect berdasarkan role
+    if(role === "admin"){
+        res.redirect('/admin');
+    } else if(role === "editor"){
+        res.redirect('/editor');
+    }
+
+});
+
+
+// Route dashboard admin
+app.get('/admin', (req, res) => {
+    res.sendFile(path.join(__dirname, 'frontend/views/dashboard_admin.html'));
+});
+
+// Route dashboard user
+app.get('/user', (req, res) => {
+    res.sendFile(path.join(__dirname, 'frontend/views/dashboard_user.html'));
+});
+
+// Server listening
+app.listen(3000, () => {
+    console.log("Server berjalan di http://localhost:3000");
+});
---
+++ backend/config/database.js
@@ -0,0 +1,19 @@
+const mysql = require("mysql2");
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "db_dashboard_artikel"
+});
+
+//test koneksi
+db.connect((err) => {
+    if (err) {
+        console.log("Koneksi database gagal: ", err.stack);
+        return;
+    } else {
+        console.log("Koneksi database berhasil!");
+    }
+});
+
+module.exports = db;
